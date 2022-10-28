e3eb72dc29edd3587c8a9661945f858d09b341d3
Mitya <116806816+EnnerDA@users.noreply.github.com>
2022-10-28 15:59:39 +0500

    Update find_smallest.py

--- find_smallest.py
+++ find_smallest.py
@@ -6,3 +6,15 @@ def find_smallest(user_list):
             smallest = user_list[i]
             smallest_index = i
     return smallest_index
+
+"""
+#TEST function
+
+from random import randint
+user_list = []
+for x in range(0, 11):
+    user_list.append(randint(1,100))
+print(user_list)
+print(f'индекс элемента с минимальным значением - {find_smallest(user_list)} \nего значение -  {user_list[find_smallest(user_list)]}')
+    
+"""
